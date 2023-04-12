499e071f24427847af9ff53a7f55d4a1c0742326
Satvik <iamsatvik77@gmail.com>
2023-04-12 19:20:53 +0530

    Trying new things

--- mini.py
+++ mini.py
@@ -1,11 +1,41 @@
+import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-class MainWidget(Widget):
-    pass
+class myGridLayout(GridLayout):
+    def __init__(self, **kwargs):
+        # calling gridlayout constructor
+        super(myGridLayout,self).__init__(**kwargs)
+
+        # setting the number of columns
+        self.cols=2
+        
+        # adding widgets
+        self.add_widget(Label(text='Name:'))
+
+        # adding the inputBox
+        self.name=TextInput(multiline=False)
+        self.add_widget(self.name)
+
+        # creating a submit button
+        self.submit=Button(text='Submit',font_size=20)
+
+        #binding the button
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self,instance):
+        name=self.name.text
+        print(name)
 
 
 class ExpenseyApp(App):
-    pass
+    def build(self):
+        return myGridLayout()
+    
 
 ExpenseyApp().run()
